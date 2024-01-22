452e5a75c3698181daa1618f67621e380d425304
icallhacks <rowandoorneweerd9@gmail.com>
2024-01-22 20:46:31 +0100

    Required Role Feature

--- commands/link.js
+++ commands/link.js
@@ -16,10 +16,17 @@ module.exports = {
         ),
     async execute(interaction) {
         const username = interaction.options.getString('username');
+        const member = interaction.member
 
         // Defer to indicate processing
         await interaction.deferReply({ ephemeral: true });
-        await this.link(interaction, interaction.member, username);
+
+        // Verify permission
+        const required_role_id = await db.get(`${interaction.guild.id}.config.required_role`)
+        if (required_role_id == null || member.roles.cache.has(required_role_id))
+            await this.link(interaction, member, username);
+        else
+            await interaction.followUp({ content: ":x: You don't have the role required to link your account." })
 
     },
 
@@ -59,7 +66,6 @@ module.exports = {
             const shouldUpdateNickname = await db.get(`${guildId}.config.set_nickname`);
             if (shouldUpdateNickname) {
                 await memberToLink.setNickname(username, "Linked").catch(err => {
-                    console.log(err)
                     interaction.followUp( { content: `:x: Your nickname was unable to be changed due to your role(s) being positioned higher than the bot's!`, ephemeral: true })
                 });
             }
